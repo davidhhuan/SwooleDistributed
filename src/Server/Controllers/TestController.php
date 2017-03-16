a0f83424aa488ef14ed257691733a14d4535a357
birdylee <birdylee_cn@163.com>
2017-03-16 20:52:05 +0800

    测试

--- src/Server/Controllers/TestController.php
+++ src/Server/Controllers/TestController.php
@@ -224,4 +224,11 @@ class TestController extends Controller
         $lock->destroy();
         $this->http_output->end(1);
     }
+    
+    public function http_testMember()
+    {
+        $model = $this->loader->model('TestModel', $this);
+        $result = yield $model->testMember();
+        $this->http_output->end($result);
+    }
 }
